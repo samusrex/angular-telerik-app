7936beeb1dacab2de7624cc315b1294ad66ed4fd
samusrex <samuelacericonia@gmail.com>
2018-10-04 16:53:06 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:  './app.component.html',
+/*styleUrls: ['./app.component.css'] */
+
 })
+
+
+@Injectable()
 export class AppComponent {
-  title = 'my-telerik';
+
+title = 'app works';
+private apiUrl = 'http://104.197.35.1:3000/serie';
+data: any =  [];
+
+public categories: number[] = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011];
+
+
+constructor  (private http:  HttpClient) {
+this.getNotes();
+this.getData();
 }
+
+getData() {
+  return this.http.get(this.apiUrl)
+    .map((res:  Response) =>  res);
+  }
+
+
+  getNotes() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+import 'hammerjs';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    ChartsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
